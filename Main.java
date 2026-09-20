e0fda456301aa563ec5d9d46c8c9b6a925e8b956
Hiu1309 <tienhieu2309@gmail.com>
2026-09-21 00:21:31 +0700

    add

--- Main.java
+++ Main.java
@@ -1,18 +1,21 @@
 import manager.QuanLyKhachHang;
-import  model.KhachHang;
+import model.KhachHang;
 import java.util.Scanner;
 
 public class Main{
     public static void main(String[] args){
-        //Từ chuỗi thành số parse
-        String s="123";
-        int a=Integer.parseInt(s);
-        System.out.println(a);
-        //Từ số thành chuỗi
-        String str=String.valueOf(a);
-        int b=Integer.parseInt(str);
-        System.out.println(b-3);
-        
+        QuanLyKhachHang dsKH = new QuanLyKhachHang();
+        Scanner sc = new Scanner(System.in);
+        KhachHang kh = new KhachHang("079", "hiu", "010");
+        dsKH.themKhachHang(kh,sc);
+        dsKH.hienThiDanhSachKhachHang();
+        dsKH.suaThongTinKhachHang(kh.getCCCD(), sc);
+        dsKH.hienThiDanhSachKhachHang();
+        dsKH.xoaKhachHang(kh.getCCCD());
+        KhachHang kh2 = new KhachHang("089", "tai", "0123");
+        dsKH.themKhachHang(kh2,sc);
+        dsKH.hienThiDanhSachKhachHang();
+       
        
     }
 }
--- manager/QuanLyKhachHang.java
+++ manager/QuanLyKhachHang.java
@@ -16,8 +16,10 @@ public class QuanLyKhachHang {
                 System.out.print("Mã khách hàng: KH"+(i+1)+
             " || ");
                 danhSachKhachHang[i].hienThiThongTin();
+                System.out.println();
             }            
         }
+        
     }
 
     public void themKhachHang(KhachHang kh, Scanner sc){        
@@ -34,8 +36,10 @@ public class QuanLyKhachHang {
             danhSachKhachHang=danhSachMoi;
         }
 
-        System.out.print("Nhập vị trí khách hàng thêm vào: ");
+        System.out.print("Nhập vị trí khách hàng muốn thêm vào: ");
         int viTri=sc.nextInt();
+        if(viTri<0||viTri>danhSachKhachHang.length)
+            return;
 
         int index=viTri-1;
 
@@ -46,11 +50,57 @@ public class QuanLyKhachHang {
 
     public int timKiemKhachHang(String cccd){
         for(int i=0;i<danhSachKhachHang.length;i++){
-            if(danhSachKhachHang[i].getCCCD().equals(cccd)){
+            if(danhSachKhachHang[i]!=null&&danhSachKhachHang[i].getCCCD().equals(cccd)){
                 return i;
             }
         }
         return -1;
+    }
+
+    public void suaThongTinKhachHang(String cccd, Scanner sc){
+        int index=timKiemKhachHang(cccd);
+        if(index==-1){
+            System.out.println("Không tìm thấy khách hàng!");
+            return;
+        }
+        KhachHang kh = danhSachKhachHang[index];
+        System.out.print("Nhập 1 để sửa toàn bộ thông tin của khách hàng\n"+
+            "Nhập 2 để sửa thông tin tùy chọn của khách hàng\nLựa chọn: ");
+        int sl=sc.nextInt();
+        sc.nextLine();
+        if(sl==1){
+            System.out.print("Số CCCD: ");
+            kh.setCCCD(sc.nextLine());
+            System.out.print("Họ và tên: ");
+            kh.setName(sc.nextLine());
+            System.out.print("Số điện thoại: ");
+            kh.setPhone(sc.nextLine());
+        }else if(sl==2){
+            System.out.print("Chọn thông tin khách hàng muốn sửa theo các số sau: "+"\n1.Để đổi số cccd"+
+                "\n2.Để đổi họ và tên\n3.Để đổi số điện thoại\nLựa chọn: ");
+            int c=sc.nextInt();
+            sc.nextLine();
+            switch(c){
+                case 1:
+                    System.out.print("Số CCCD: ");
+                    kh.setCCCD(sc.nextLine());
+                    break;
+                case 2:
+                    System.out.print("Họ và tên: ");
+                    kh.setName(sc.nextLine());
+                    break;
+                case 3:
+                    System.out.print("Số điện thoại: ");
+                    kh.setPhone(sc.nextLine());
+                    break;
+                default:
+                    System.out.println("Lựa chọn không hợp lệ!");
+                    break;
+            }
+        }else{
+            System.out.println("Lựa chọn không hợp lệ!");
+        }
+        danhSachKhachHang[index]=kh;
     }
 
     public void xoaKhachHang(String cccd){        
@@ -65,7 +115,4 @@ public class QuanLyKhachHang {
         
     }
   
-
-
-
 }
